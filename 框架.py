70112339b5c80ceb4fd45591454be33a8d1776cd
sephinawith <sephina111@gmail.com>
2026-05-03 15:21:57 +0800

    changed initial

--- 框架.py
+++ 框架.py
@@ -4,5 +4,11 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
+# 1. 先在这里定义 main 函数
+def main():
+    # 你接下来要写的读取csv、训练决策树的代码，都缩进写在这个函数里面
+    pass
+
+# 2. 然后再调用它
 if __name__ == '__main__':
     main()
