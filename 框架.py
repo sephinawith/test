02cc53f9150e1c7dbe1e7bf65e0e2e8d1db9c334
sephinawith <sephina111@gmail.com>
2026-05-03 14:20:12 +0800

    initial commit

---
+++ 框架.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+if __name__ == '__main__':
+    main()
